d25bf3b5691260e5784ab7569fe0067c892846d3
Danuel <public.danuel@gmail.com>
2020-03-28 16:20:49 +0900

    setup project structures

--- app/Pages/index.tsx
+++ app/Pages/index.tsx
@@ -1,3 +1,10 @@
 import React from "react";
+import { Route, Switch } from "react-router";
+import { MAIN } from "../constants/Route";
+import { Main } from "./Main";
 
-export const Pages = () => <div>Pages</div>;
+export const Pages = () => (
+  <Switch>
+    <Route path={MAIN} component={Main} />
+  </Switch>
+);
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,24 @@
 import React, { Fragment } from "react";
 import { render } from "react-dom";
 import { AppContainer as ReactHotAppContainer } from "react-hot-loader";
-import { Pages } from "./Pages";
+import { BrowserRouter } from "react-router-dom";
+import { Pages } from "./pages";
+import "./styles";
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+
+const App = () => (
+  <BrowserRouter>
+    <Pages />
+  </BrowserRouter>
+);
 
 declare const Root: HTMLDivElement;
 
 document.addEventListener("DOMContentLoaded", () =>
   render(
     <AppContainer>
-      <Pages />
+      <App />
     </AppContainer>,
     Root
   )
